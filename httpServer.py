17f3b7df49b9ecb9f66ced06ee4b21054ef0f2ca
Ian Davis <misterminecrafterdude@gmail.com>
2021-03-22 10:00:30 -0400

    Added code resources
    Added all the HTML, CSS, resources, and python server code

---
+++ httpServer.py
@@ -0,0 +1,13 @@
+import http.server
+import socketserver
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = 'home.html'
+        elif self.path == '/info':
+            self.path = 'info.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+handler_object = MyHttpRequestHandler
+PORT = 8080
+my_server = socketserver.TCPServer(("", PORT), handler_object)
+my_server.serve_forever()
